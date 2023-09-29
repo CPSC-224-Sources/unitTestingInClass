1e578130317a4878dcbb8b180fda03e2bae96d65
Aaron S. Crandall <crandall@gonzaga.edu>
2023-09-29 10:43:51 -0700

    Adding some debit/credit transactions and some testing demos

--- src/edu/gonzaga/BankAccount.java
+++ src/edu/gonzaga/BankAccount.java
@@ -1,5 +1,49 @@
 package edu.gonzaga;
 
+/*
+ * 
+ */
+abstract class Transaction {
+    protected String typeName = "";
+    protected Double amount = 0.0;
+    private static Integer idCounter = 1;
+    private Integer uniqueID = 0;
+
+    public Transaction() {
+        this.uniqueID = idCounter++;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction (" + this.uniqueID + ") " + typeName + " | amount: $" + amount;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+}
+
+class Credit extends Transaction {
+    public Credit(Double amount) {
+        super();
+        this.typeName = "credit";
+        this.amount = amount;
+    }
+}
+
+class Debit extends Transaction {
+    public Debit(Double amount) {
+        super();
+        this.typeName = "debit";
+        this.amount = amount;
+    }
+}
+
+
+
+/*
+ * 
+ */
 public class BankAccount {
     private Double balance;
     private String name;
@@ -37,3 +81,19 @@ public class BankAccount {
         this.balance -= spentFunds;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+/*
+        if(newFunds < 0) {
+        throw new IllegalArgumentException("Cannot credit negative amounts.");
+        }
+ */
--- test/edu/gonzaga/BankAccountTest.java
+++ test/edu/gonzaga/BankAccountTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BankAccountTest {
+    //@Rule
+    //public final ExpectedException exception = ExpectedException.none();
+
     @Test
     void initialBalance() {
         Double initialBal = 50.75;
@@ -12,4 +15,30 @@ public class BankAccountTest {
         System.out.println(ba);
         Assertions.assertEquals(initialBal, ba.getBalance());
     }
+
+    @Test
+    void test_CreateCredit() {
+        Credit credit = new Credit(50.0);
+        System.out.println(credit);
+    }
+
+    @Test
+    void test_CreateDebit() {
+        Debit debit = new Debit(70.0);
+        System.out.println(debit);
+    }
+
+
+    /*
+    @Test
+    void negativeCredit() {
+        Double initialBal = 50.75;
+        Double negativeAmount = -1.5;
+        String ownerName = "Jenny";
+        BankAccount ba = new BankAccount(initialBal, ownerName);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            ba.credit(negativeAmount);
+        });
+    }
+    */
 }
